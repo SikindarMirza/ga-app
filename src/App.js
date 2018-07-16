dd50d9c2be92ecea4f23b8556ebe3f39079e901b
INE12360129 <amit.mastud@tesco.com>
2018-07-16 18:33:54 +0530

    Fixed the errors

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
     ReactGA.initialize('UA-122417377-1');
     ReactGA.pageview(window.location.pathname);
-
-    handleClick(){
-      ReactGA.event({
-      category: 'Submit',
-      action: 'Clicked button',
-    });
-    }
   }
+  handleClick() {
+    ReactGA.event({
+    category: 'Submit',
+    action: 'Clicked button'
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <p className="message">
           Google Analytics
         </p>
-        <button type="button" onClick={() =>{this.handleClick()}}>Click me!</button>
+        <button type="button" onClick={this.handleClick()}>Click me!</button>
       </div>
     );
   }
