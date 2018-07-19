c61891533f9b58c7baeb71d15cdcd54c0f9e2b7a
INE12360129 <amit.mastud@tesco.com>
2018-07-20 03:33:45 +0530

    Added instafeed

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+<script>
+  <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/59a5899fa3bc59c3a84644c59467fcb0.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+</script>
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
